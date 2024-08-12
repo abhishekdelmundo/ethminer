ffa904d052505c5c60df1ab222a924391c5ce3d7
abhishekdelmundo <137968400+abhishekdelmundo@users.noreply.github.com>
2024-08-12 22:32:17 +0530

    Add files via upload

---
+++ start_miner.sh
@@ -0,0 +1,6 @@
+#
+# Example shell file for starting PhoenixMiner.exe to mine ETH
+#
+
+# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+./PhoenixMiner -pool ssl://asia1-etc.ethermine.org:4444 -pool2 ssl://eu1-etc.ethermine.org:4444 -wal 0xb4162b8E61B058646aE9Fd5befFDbA333D7E575b.Rig001
